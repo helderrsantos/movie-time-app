215e265f8aeb3cc9fef50d22a184e4383c1e1cb6
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-08-22 23:13:30 -0300

    chore: added lib bottom tabs, for better navigation but needs tweaking

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,15 +4,17 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Splash } from '../screens/Splash';
 import { Home } from '../screens/Home';
 import { Details } from '../screens/Details';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Entypo } from '@expo/vector-icons';
 
-const { Navigator, Screen} = createNativeStackNavigator();
+const { Navigator, Screen} = createBottomTabNavigator();
 
 function Routes() {
     return (
       <NavigationContainer>
         <Navigator initialRouteName="Splash" screenOptions={{headerShown:false}}>
           <Screen name="Splash" component={Splash} />
-          <Screen name="Home" component={Home} />
+          <Screen name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<Entypo name="home" size={26} color="black" /> ),}}/>
           <Screen name="Details" component={Details} />
         </Navigator>
       </NavigationContainer>
