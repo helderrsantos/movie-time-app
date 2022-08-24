47eae0064a08b52977fc24b434b7759d184e8c84
Helder Rodrigo <helderr_santosd@hotmail.com>
2022-08-24 17:15:18 -0300

    feat: added to the home screen the bottom tabs for navigation between pages, but needs improvement

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,46 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Splash } from '../screens/Splash';
 import { Home } from '../screens/Home';
 import { Details } from '../screens/Details';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
+import GlobalStyle from '../styles/theme';
+import { SearchBar,  } from 'react-native-screens';
 
-const { Navigator, Screen} = createBottomTabNavigator();
+const { Navigator: NavigatorStack, Screen: StackScreen } = createNativeStackNavigator();
+const { Navigator: NavigatorTab, Screen: TabScreen } = createBottomTabNavigator();
+
+function Tabs() {
+  return (
+    <NavigatorTab screenOptions={{ 
+        headerShown: false, 
+        tabBarActiveTintColor: GlobalStyle.colors.primary,
+        tabBarInactiveTintColor: GlobalStyle.colors.highlight,
+        tabBarStyle: { backgroundColor: GlobalStyle.colors.overlay }
+      }}>
+      <TabScreen name="Home" component={Home} options={{ tabBarIcon: ({ color, size }) => (<Feather name="home" size={size} color={color} /> ) }}/>
+      <TabScreen name="Search" component={SearchBar} options={{ tabBarIcon: ({ color, size }) => (<Feather name="search" size={size} color={color} /> ) }}/>
+      <TabScreen name="Menu" component={Splash} options={{ tabBarIcon: ({ color, size }) => (<Feather name="menu" size={size} color={color} /> ) }}/>
+    </NavigatorTab>
+  );
+}
 
 function Routes() {
-    return (
-      <NavigationContainer>
-        <Navigator initialRouteName="Splash" screenOptions={{headerShown:false}}>
-          <Screen name="Splash" component={Splash} />
-          <Screen name="Home" component={Home} options={{tabBarIcon: ({ color, size }) => (<Entypo name="home" size={26} color="black" /> ),}}/>
-          <Screen name="Details" component={Details} />
-        </Navigator>
-      </NavigationContainer>
-    );
-  }
+  return (
+    <NavigationContainer>
+      <NavigatorStack initialRouteName='Splash' screenOptions={{ headerShown: false }}>
+        <StackScreen name="Splash" component={Splash} />
+        <StackScreen
+          name="Home"
+          component={Tabs}
+          options={{ headerShown: false }}
+        />
+        <StackScreen name="Details" component={Details} />
+      </NavigatorStack>
+    </NavigationContainer>
+  );
+}
 
-  export default Routes;
+export default Routes;
